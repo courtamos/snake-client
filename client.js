057b947dd00398406266eedc9407fe6cb2046ea0
Courtney Amos <c.amos@shaw.ca>
2021-04-07 15:44:26 -0700

    created constants.js to hold consts that are exported for the app

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   conn.on('connect', () => { // msg showing connection to server
