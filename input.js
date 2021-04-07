69854bc6a035694cb5cf68dbe4fadbf3e91b7f49
Courtney Amos <c.amos@shaw.ca>
2021-04-07 14:30:37 -0700

    implemented WASD movement for snake by sending data from input mode

--- input.js
+++ input.js
@@ -1,24 +1,41 @@
+let connection;
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', (key) => handleUserInput(key)); // on data input/key being pushed, call handleUserInput with the key being pushed
 
-  const handleUserInput = function(key) { // once called from stdin.on.. execute this block
-    if (key === '\u0003') { // \u0003 maps to ctrl+c input
-      console.log('exiting now');
-      process.exit();
-    }
-  };
+  stdin.on('data', (key) => { // on data input/key being pushed, call handleUserInput with the key being pushed
+    handleUserInput(key);
+  });
 
   return stdin;
 };
 
 setupInput(); // calling function
 
-module.exports = { setupInput };
+const handleUserInput = (key) => { // once called by key, execute this block
+  if (key === '\u0003') { // \u0003 maps to ctrl+c input
+    console.log('exiting now...');
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+};
+
+module.exports = { setupInput }; // exporting setupInput into a module
