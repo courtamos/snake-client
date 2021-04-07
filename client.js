c0d69633c65c769dca5be444184ced65d0952426
Courtney Amos <c.amos@shaw.ca>
2021-04-06 20:43:10 -0700

    implemented hard code for telling snake when and what direction to move (commented out now)

--- client.js
+++ client.js
@@ -8,13 +8,20 @@ const connect = function() {
     port: 50541
   });
 
-  conn.on('connect', () => {
+  conn.on('connect', () => { // msg showing connection to server
     console.log('Successfully connected to game server');
   });
 
-  conn.on('connect', () => {
+  conn.on('connect', () => { // sharing name with server
     conn.write('Name: CRT');
   });
+
+  // conn.on('connect', () => { // telling server to move snake
+  //   setInterval(() => conn.write('Move: down'), 1000);
+  //   // setTimeout(() => conn.write('Move: left'), 2000);
+  //   // setTimeout(() => conn.write('Move: up'), 3000);
+    
+  // });
 
   conn.on('data', (data) => { // event handlers to handle income data and console.log it for the player
     console.log('Server says: ', data);
