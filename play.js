ba23acde78253cf281044c2702f5b61a87404598
Courtney Amos <c.amos@shaw.ca>
2021-04-07 08:56:41 -0700

    created an input module, input.js, to import and call the function from the play.js file

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (key) => handleUserInput(key)); // on data input/key being pushed, call handleUserInput with the key being pushed
 
-  const handleUserInput = function(key) { // once called from stdin.on.. execute this block
-    if (key === '\u0003') { // \u0003 maps to ctrl+c input
-      console.log('exiting now');
-      process.exit();
-    }
-  };
-
-  return stdin;
-};
-
-setupInput(); // calling function
+setupInput();
